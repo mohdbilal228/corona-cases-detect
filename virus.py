cb7b624cb3a774a10f7aece5438a4ff4fbad0fe7
mohdbilal228 <alexmercerham228@gmail.com>
2020-08-29 20:29:14 +0530

    easy program for covid 19 cases

--- virus.py
+++ virus.py
@@ -1,1 +1,11 @@
-
+#importing Covid from Covid library
+from covid import Covid
+#intialization Covid()
+#covid is variable using for intializating
+covid = Covid()
+#printing the status of country by country name,eg:india, using covid.  type of inbuild funct
+cases = covid.get_status_by_country_name("india")
+#output is in dictionary form so using for loob for printing in standard form
+for x in cases:
+#using print function
+print(x,"=",cases[x])
